62ad36675f87f26a1ac465b1e55f228d37bfe30b
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2024-01-28 04:11:10 +0500

    working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`$(inter.className bg-myGreen)`}>
+      <body className={`${inter.className} bg-red-700`}>
         <Navbar />
         {children}</body>
     </html>
