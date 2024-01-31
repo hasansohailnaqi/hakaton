b2334ba0197fab02a61ce70a3c63936a33438061
Hasan Sohail <138737153+hasansohailnaqi@users.noreply.github.com>
2024-01-31 19:21:38 +0500

    working

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
    </div>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    {/* <a className="btn bg-white">Button</a> */}
   </div>
 </div>
     </div>
